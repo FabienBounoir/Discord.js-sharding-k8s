15c008b0aacb0614d23ad85b4adbd74a7b8915e7
Fabien Bounoir <71253960+FabienBounoir@users.noreply.github.com>
2023-09-27 13:32:33 +0000

    💫 Add log to check

--- bot.js
+++ bot.js
@@ -1,12 +1,18 @@
 // bot.js
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent,GatewayIntentBits.GuildMessages] });
 
 client.on(Events.MessageCreate, message => {
+	console.log(message)
 	if (message.content === '!bot') {
 		return message.reply({content:`Hello word`})
 	}
 });
+
+client.on(Events.ClientReady, () => {
+	console.log("Guilds: "+client.guilds.cache.size)
+});
+
 
 client.login(process.env.TOKEN);
